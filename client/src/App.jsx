4d9fcdfe347bc38e01e6c786ffbb2ff61e36ebb7
Mingxia Zeng <emiliezeng91@gmail.com>
2024-08-28 00:33:05 -0400

    add socket.io

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,1 +1,19 @@
 import './App.css';
+import { useEffect } from 'react';
+import { io } from 'socket.io-client';
+
+function App() {
+  useEffect(() => {
+    const socket = io('http://localhost:3000');
+    socket.on('connect', () => {
+      console.log('connected to server');
+    });
+  }, []);
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,31 @@
 import express from "express";
-
+import { Server } from "socket.io";
+import { createServer } from "node:http";
+import cors from 'cors';
 const app = express(); 
 
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["*"],
+        credentials: true
+    }
+});
 
-app.listen(3000, function () {
+app.use(cors());
+app.get('/', (req, res) => {
+    res.send('Hello World');
+} );
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
+} );
+
+server.listen(3000, function () {
     console.log('listening on port 3000');
 } )
