65e4160ec0072299f78addd5fe7250c9c7a94d42
Mingxia Zeng <emiliezeng91@gmail.com>
2024-08-28 16:04:19 -0400

    style: refine style

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -7,6 +7,7 @@ export default function Header({user}) {
 
     return (
         <div className="header">
+            <h2>Cat Chat</h2>
             <Link to="/" className="header__logo"><CatFace/></Link>
             <div className="header__username">{user?.username ? `Cat name: ${user.username}`: "You are not a cat yet."}</div>
         </div>
